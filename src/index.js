ae6f2f0ce52795270f02d4464266e017c63c2d23
Joshua Purcell <jlpurcell@us.ibm.com>
2016-06-29 12:44:35 -0700

    add src subdirectories, cfignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Store from './Store'
-import App from './App'
+import Store from './stores/Store'
+import App from './components/App/App'
 
 const store = new Store()
 render(<App store={store} />,document.getElementById('root'))
